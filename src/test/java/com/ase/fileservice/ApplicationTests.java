990a30741073b74f1679d2860fee99eeaffe1354
Sven <sven.boerrigter@stud-provadis-hochschule.de>
2025-08-20 14:52:32 +0200

    Barebone Files Controller

--- src/test/java/com/ase/fileservice/ApplicationTests.java
+++ src/test/java/com/ase/fileservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ase.userservice;
+package com.ase.fileservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
