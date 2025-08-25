2de5e3d9ff056b315cc8d3b22ae81b9f16fa9406
Snens <153861591+xxtrixxsnens@users.noreply.github.com>
2025-08-25 12:43:23 +0200

    feat: ase-338-API-Design (#2)
    API Design.
    
     API-Design für Datei-Upload, -Download, -Verwaltung.
    Konsistente Schnittstelle für externe Systeme.

--- src/main/java/com/ase/fileservice/Application.java
+++ src/main/java/com/ase/fileservice/Application.java
@@ -1,4 +1,4 @@
-package com.ase.userservice;
+package com.ase.fileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/ase/fileservice/controllers/FilesController.java
@@ -0,0 +1,9 @@
+package com.ase.fileservice.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.ase.fileservice.api.FilesApi;
+
+@RestController
+public class FilesController implements FilesApi {
+
+}
